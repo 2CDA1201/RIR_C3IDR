a9941704270290984535832666af9c8d6e4782da
Hushade <uki.110m.saito@gmail.com>
2025-09-27 09:28:59 +0900

    httpからの取得を非同期処理に変更

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import os
-import requests
+import asyncio
+import aiohttp
+import aiofiles
 
 RIR_DICT = {
     "ARIN": {
@@ -29,50 +31,58 @@ RIR_DICT = {
     },
 }
 
-def retr_https(region):
+async def fetch(session, url):
+    async with session.get(url) as response:
+        response.raise_for_status()
+        return await response.read()
+
+async def fetch_rir(session, region):
     base_url = region["base_url"]
     filename = region["file"]
     region_name = region["region"]
 
     try:
-        print(f"Checking {region_name} ...")
+        print(f"Checking updates for {region_name} ...")
 
         # md5 の取得
         md5_url = f"{base_url}{filename}.md5"
-        r = requests.get(md5_url, timeout=30)
-        r.raise_for_status()
-        remote_md5 = r.content
+        remote_md5 = await fetch(session, md5_url)
 
         local_md5_path = f"data/{filename}.md5"
         local_file_path = f"data/{filename}.psv"
 
         if os.path.exists(local_md5_path):
-            with open(local_md5_path, "rb") as fp:
-                if remote_md5 == fp.read():
+            async with aiofiles.open(local_md5_path, "rb") as fp:
+                if remote_md5 == await fp.read():
                     print(f"No update for {filename}, skipping download.")
                     return
 
         # データ本体を取得
         data_url = f"{base_url}{filename}"
-        print(f"Retrieving {filename} ...")
-        r = requests.get(data_url, timeout=60)
-        r.raise_for_status()
-        with open(local_file_path, "wb") as fp:
-            fp.write(r.content)
+        print(f"Fetching {filename} ...")
+        data = await fetch(session, data_url)
+
+        # データの保存
+        async with aiofiles.open(local_file_path, "wb") as fp:
+            await fp.write(data)
 
         # md5 の保存
-        with open(local_md5_path, "wb") as fp:
-            fp.write(remote_md5)
+        async with aiofiles.open(local_md5_path, "wb") as fp:
+            await fp.write(remote_md5)
 
         print(f"Updated {filename}")
 
     except Exception as e:
         print(f"Error accessing {base_url}: {str(e)}")
 
-def main():
+async def main():
     os.makedirs("data", exist_ok=True)
-    for stats in RIR_DICT.values():
-        retr_https(stats)
+    
+    timeout = aiohttp.ClientTimeout(total=60)
+    async with aiohttp.ClientSession(timeout=timeout) as session:
+        tasks = [fetch_rir(session, stats) for stats in RIR_DICT.values()]
+        await asyncio.gather(*tasks)
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
+    print("Fetching RIR data completed.")
